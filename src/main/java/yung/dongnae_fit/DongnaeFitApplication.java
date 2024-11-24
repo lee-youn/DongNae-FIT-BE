2a2e0b5ffaa8b8785dc314fb7ead62f871f425b2
lee-youn <5816654@naver.com>
2024-11-25 04:07:45 +0900

    프로젝트 초기설정

---
+++ src/main/java/yung/dongnae_fit/DongnaeFitApplication.java
@@ -0,0 +1,13 @@
+package yung.dongnae_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongnaeFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongnaeFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yung/dongnae_fit/DongnaeFitApplicationTests.java
@@ -0,0 +1,13 @@
+package yung.dongnae_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongnaeFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
