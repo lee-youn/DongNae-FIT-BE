57885ed3da1cdc358741055e62a39dcd38923412
lee-youn <5816654@naver.com>
2024-11-30 02:11:08 +0900

    Feat : #1 소셜 로그인 코드 구현

--- src/main/java/yung/dongnae_fit/DongnaeFitApplication.java
+++ src/main/java/yung/dongnae_fit/DongnaeFitApplication.java
@@ -2,8 +2,10 @@ package yung.dongnae_fit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DongnaeFitApplication {
 
 	public static void main(String[] args) {
