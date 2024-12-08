ca320f711f39d7cee90593e97b9f74e0e9fc14e1
lee-youn <5816654@naver.com>
2024-12-08 15:47:37 +0900

    Feat : #3 post date 추가

--- src/main/java/yung/dongnae_fit/domain/post/dto/PostListResponseDTO.java
+++ src/main/java/yung/dongnae_fit/domain/post/dto/PostListResponseDTO.java
@@ -5,12 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import yung.dongnae_fit.domain.post.entity.Post;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 public class PostListResponseDTO {
     private Long postId;
     private String postTitle;
     private String postDetail;
+    private LocalDateTime postDate;
     private String postImage;
     private Long postLikeCount;
     private Long postSaveCount;
@@ -21,6 +24,7 @@ public class PostListResponseDTO {
         this.postTitle = post.getTitle();
         this.postDetail = post.getDetail();
         this.postImage = post.getImage();
+        this.postDate = post.getDate();
         this.postLikeCount = (long) post.getPostLikes().size();
         this.postSaveCount = (long) post.getPostSaves().size();
     }
